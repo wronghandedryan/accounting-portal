cf34e28be53e7384e202a0008c14e9facc7c3db8
Ryan O'Boyle <wronghanded@protonmail.com>
2020-07-07 17:39:57 -0700

    last try before class

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
 
-const PORT = ${PORT: process.env};
+const PORT = ${PORT};
 
 const app = express();
 
